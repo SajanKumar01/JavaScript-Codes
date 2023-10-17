505be99204bcbc775eaceebf87242001cc80f3b6
SajanKumar01 <sajanrajak9570@gmail.com>
2023-10-17 20:51:22 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,27 @@
+//  !   01 variables in javascripts 
+
+const accountId = 203030500
+let accountEmail = "sajan73@gmail.com"
+var accountPasswords = "784569"
+accountCity = "Bokaro"
+
+// accountId = 4857  // this is not allowed
+// const mai change nahi hota hai  kuch 
+
+accountEmail = "milan@gmail.com"
+accountPasswords = 5695
+accountCity = "Dhanbad"
+
+console.log(accountCity);
+
+console.log(accountEmail,accountPasswords,accountCity);
+
+console.table([accountEmail,accountPasswords,accountCity]);
+
+
+
+// keywords in javascript
+
+// 1. const
+// 2. let
+// 3. var
